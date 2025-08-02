d4105079b2c8113344dcbb4ab1003a4226892285
CodereaperXE <rishab.budale@gmail.com>
2025-08-02 14:35:39 +0530

    corrected arithmetic right shift bug

--- src/alu.v
+++ src/alu.v
@@ -14,17 +14,21 @@ assign zero = ((op1-op2)==32'd0) ? 1 : 0;
 assign negative = ((op1 - op2) & 32'h80000000) ? 1 : 0;
 assign unegative = (op1 < op2) ? 1 : 0; //used for bltu and bgeu
 
-assign res = (alu_sel==4'b0000) ? op1+op2 :
-             (alu_sel==4'b0001) ? op1-op2 : 
+//for arithmetic shift instructions
+wire signed [31:0] s_op1 = op1;
+wire signed [31:0] sra_result = s_op1 >>> op2;
+
+assign res = (alu_sel==4'b0000) ? op1+op2 : //add or addi
+             (alu_sel==4'b0001) ? op1-op2 : //sub
              (alu_sel==4'b0010) ? op2 : //send only op2 for lui
-             (alu_sel==4'b0011) ? op1 << op2 : //left shift
-             (alu_sel==4'b0100) ? op1 >> op2 : //right shift
-             (alu_sel==4'b0101) ? $signed(op1) >>> op2 : //arithmetic right shift
-             (alu_sel==4'b0110) ? op1 ^ op2 : //xor
-             (alu_sel==4'b0111) ? op1 | op2 : //or
-             (alu_sel==4'b1000) ? op1 & op2 : //and
-             (alu_sel==4'b1001) ? ($signed(op1) < $signed(op2)) ? 1 : 0 : //slti
-             (alu_sel==4'b1010) ? ((op1 < op2) ? 1 : 0): //sltiu
+             (alu_sel==4'b0011) ? op1 << op2 : //left shift (assumming compiler takes care of selecting operand op2[4:0] in R type instructions)
+             (alu_sel==4'b0100) ? op1 >> op2 : //right shift (assumming compiler takes care of selecting operand op2[4:0] in R type instructions)
+             (alu_sel==4'b0101) ? sra_result : //arithmetic right shift (assumming compiler takes care of selecting operand op2[4:0] in R type instructions)
+             (alu_sel==4'b0110) ? op1 ^ op2 : //xor or xori
+             (alu_sel==4'b0111) ? op1 | op2 : //or or ori
+             (alu_sel==4'b1000) ? op1 & op2 : //and or andi
+             (alu_sel==4'b1001) ? ($signed(op1) < $signed(op2)) ? 1 : 0 : //slti or slt
+             (alu_sel==4'b1010) ? ((op1 < op2) ? 1 : 0): //sltiu or sltu
              32'd0;
 endmodule
 
